5268211eb3850462eb47dd148cfb68582e1abe40
Prathamesh more <91453437+iamprathameshmore@users.noreply.github.com>
2023-07-12 01:00:25 +0530

    Add files via upload

---
+++ problem01.js
@@ -0,0 +1,13 @@
+// Write a JavaScript program to find the area of a triangle where three sides are 5, 6, 7.  
+
+
+let a = 5;
+let b = 6;
+let c = 7;
+let s = (a+b+c)/2;
+let area_of_triangle = Math.sqrt(s*((s-a)*(s-b)*(s-c)));
+
+console.log(" area_of_triangle = " + area_of_triangle)  
+
+// ansewer are given below 
+// area_of_triangle = 14.696938456699069
